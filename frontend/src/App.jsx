9d2753cd364e83d20d8eb2b28e07e1d54e147ea5
Hugo Carnazza <hugo.azzanrac@gmail.com>
2023-05-11 18:33:36 +0200

    searchbar finish without css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import React, { useState } from "react";
 import Home from "./pages/Home";
 import "./reset.css";
 import "./App.css";
 
 function App() {
+  const [communeSelectedAdd, setCommuneSelectedAdd] = useState([]);
+  const [cityDataSearch, setCityDataSearch] = useState([]);
   return (
     <div className="App">
-      <Home />
+      <Home
+        communeSelectedAdd={communeSelectedAdd}
+        setCommuneSelectedAdd={setCommuneSelectedAdd}
+        cityDataSearch={cityDataSearch}
+        setCityDataSearch={setCityDataSearch}
+      />
       <p>coucou</p>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,34 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable react/prop-types */
 import React from "react";
+import Searchbar from "../components/searchbar/Searchbar";
 
-function Home() {
-  return <div>coucou 1</div>;
+function Home({
+  communeSelectedAdd,
+  setCommuneSelectedAdd,
+  cityDataSearch,
+  setCityDataSearch,
+}) {
+  return (
+    <div>
+      {cityDataSearch}
+      <div className="SearchBar">
+        <Searchbar
+          setCommuneSelectedAdd={setCommuneSelectedAdd}
+          communeSelectedAdd={communeSelectedAdd}
+          setCityDataSearch={setCityDataSearch}
+        />
+
+        <div>
+          <ul>
+            {communeSelectedAdd.map((name) => (
+              <li key={name}>{name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
